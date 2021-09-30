5ca8d639d73e09843897a002632b774bcf0f4e5b
Yana <yanaegorova69.3@gmail.com>
2021-09-30 22:46:39 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,169 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Teether Toy',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                title: 'Soft Silicone',
+                text: 'Non-toxic food grade silicone and is free of BPA, PVC, phthalates, lead and cadmium.',
+            },
+            {
+                title: 'Berrybumps',
+                text: 'Patented bumps soothe baby\'s swollen gums and emerging teeth. Perfect texture to chew.',
+            },
+            {
+                title: 'Original',
+                text: 'Is the original hands free teething pacifier for babies.',
+            },
+            {
+                title: 'Perfectly Sized',
+                text: 'Perfectly shaped and sized for your little one\'s hands. Ages 3 months plus.',
+            },
+            {
+                title: 'Multi-Texture',
+                text: 'Multi Texture Teething surfaces encourage baby teething and grabbing.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Silverware Set',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: '20 Pcs Flatware Sets Service For 4',
+                text: 'This silverware set includes 4 dessert fork, 4 dessert spoons, 4 dinner forks, 4 dinner spoons, and 4 dinner knives,It can satisfy most family of 4 people.',
+            },
+            {
+                title: 'High Quality Material',
+                text: 'Extra thick refine high quality stainless steel,matte finish, excellent resistance against corrosion,extremely durable.',
+            },
+            {
+                title: 'Superior Design',
+                text: 'Smooth surface and satin polish makes these tableware sets always keep shining,simple classic modern design would match well with your existing cutlery and elevate everyday meals into a fine dining experience.',
+            },
+            {
+                title: 'Durable And Safe',
+                text: 'The silverware is dishwasher safe,also easy to hand wash,your proper care can make the utensil sets always looks as good as new, and enable it to last for many years.',
+            },
+            {
+                title: 'Multi Use',
+                text: 'Modern and elegant flatware set perfect for wedding, Christmas party, Gathering, Restaurant, Appetite increased and more enjoyable dinning experiences awaits, Also as a great gift .',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Single Horn Anvil',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Premium Material',
+                text: 'This anvil is made of high-grade cast iron for high hardness, maximum strength and durability. It withstands heavy blows.',
+            },
+            {
+                title: 'Functional Structure',
+                text: 'Rounded horn for smooth bending and shaping, the flat surface for punching. Solid base keeps sturdy.',
+            },
+            {
+                title: 'Surface Treatment',
+                text: 'Fully polished surface area, through quenching treatment, the spray coating effectively relieves the oxidation reaction.',
+            },
+            {
+                title: 'Large Woking Area',
+                text: 'This blacksmith forge comes with comforatble and wide work surface, giving large working area. Weighing 24 pounds (11 kg), the anvil can be transported easily.',
+            },
+            {
+                title: 'Wide Application',
+                text: 'This anvil is perfect for metal-smiths, riveting, flattening, forging and forming metal, ideal for any handyman or hobbyist. It\'s a great tool for crafting small stuff like silversmithing.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.png', './img/img3-5.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Multipurpose Vise',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: '360 Degrees Rotation',
+                text: 'The head of the vise can be rotated by 360 degrees for extra versatility, and the swivel base of the vise allows for optimal work placement and flexibility.',
+            },
+            {
+                title: 'Sufficient Clamping Capacity',
+                text: 'The built-in jaws are grooved for enhanced grip and allow for easy clamping of pipe and other rounded material.',
+            },
+            {
+                title: 'Heavy-Duty Construction',
+                text: 'Made of ductile iron which increases the durability and longevity, this table vise is designed to be used in a tough environment.',
+            },
+            {
+                title: 'Precision & Hardness',
+                text: 'Polishing lead screw and guide rail are upgraded to achieve high precision and high hardness. The workbench vise can clamp and tighten quickly and smoothly after precise grinding.',
+            },
+            {
+                title: 'Multifunctional Application',
+                text: 'Widely used in the industry or household. Mounting vise on the workbench, then you could do the work of grinding, clamping, and fixing, etc.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Waffle Iron',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Creates two deep-pocket 1-inch Belgian waffles at the same time.',
+            },
+            {
+                text: 'Dimensions : 15. 50 L x 9. 75 W x 9. 30 H inches. Cord length : 36 Inches.',
+            },
+            {
+                text: '6-setting browning control knob ensures each waffle is cooked to order.',
+            },
+            {
+                text: 'Nonstick coating for quick food release and easy cleanup.',
+            },
+            {
+                text: '2 LED ready indicator lights and 3 audio beep tones signal when each waffle is done.',
+            },
+            {
+                text: '1400 watts of power. UC Cubic Feet: 1. 08.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Kids Table and Bench',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Kids’ picnic-style table for eating, playing, art activities, and more.',
+            },
+            {
+                text: 'Perfect height and size for children.',
+            },
+            {
+                text: 'Includes rectangular table and two matching benches.',
+            },
+            {
+                text: 'Made of durable, natural solid wood.',
+            },
+            {
+                text: 'Dynamic use in playrooms, bedrooms, kitchens, living rooms, and more.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+]
